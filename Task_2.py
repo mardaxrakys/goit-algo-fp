beeb90173509881f1f0b7ec0a1cc00428da96e4a
mardaxrakys <163344559+mardaxrakys@users.noreply.github.com>
2024-08-21 20:06:29 +0300

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,57 +1,47 @@
 import turtle
-import math
 
-def draw_pythagoras_tree(t, length, level):
+def draw_tree(t, branch_length, level, angle):
     if level == 0:
         return
 
-    # Малюємо квадрат
-    for _ in range(4):
-        t.forward(length)
-        t.left(90)
-
+    # Малюємо гілку
+    t.forward(branch_length)
+    
     # Зберігаємо поточну позицію та напрямок
     pos = t.pos()
-    angle = t.heading()
-
-    # Розрахунок довжин сторін нових квадратів
-    length_new = length * math.sqrt(2) / 2
+    heading = t.heading()
 
     # Малюємо ліву гілку
-    t.left(45)
-    t.forward(length_new)
-    draw_pythagoras_tree(t, length_new, level - 1)
+    t.left(angle)
+    draw_tree(t, branch_length * 0.7, level - 1, angle)
 
-    # Повертаємося до позиції батьківського квадрата
+    # Повертаємося до позиції батьківської гілки
     t.setpos(pos)
-    t.setheading(angle)
+    t.setheading(heading)
 
     # Малюємо праву гілку
-    t.forward(length)
-    t.right(45)
-    t.forward(length_new)
-    draw_pythagoras_tree(t, length_new, level - 1)
+    t.right(angle)
+    draw_tree(t, branch_length * 0.7, level - 1, angle)
 
     # Повертаємося до початкової позиції та напрямку
     t.setpos(pos)
-    t.setheading(angle)
+    t.setheading(heading)
 
 def main():
     screen = turtle.Screen()
     screen.bgcolor("white")
     t = turtle.Turtle()
     t.speed("fastest")
-
-    # Вводимо рівень рекурсії
-    level = int(input("Введіть рівень рекурсії (рекомендується від 1 до 10): "))
+    t.pencolor("brown")
 
     # Встановлюємо початкову позицію
     t.penup()
-    t.goto(-150, -200)
+    t.goto(0, -250)
+    t.left(90)
     t.pendown()
 
-    # Малюємо фрактал
-    draw_pythagoras_tree(t, 100, level)
+    # Малюємо дерево
+    draw_tree(t, 100, 9, 30)
 
     # Завершуємо роботу Turtle
     turtle.done()
